30de8e5cf53c520c3efc75feab6334d6ce737302
Bill Kirtley <bill@spiffy.com>
2016-01-05 13:32:15 -0500

    Workaround for fixture loading
    ref. rails/rails#22584

---
+++ config/initializers/patch_issue_22584.rb
@@ -0,0 +1,12 @@
+# Temporary patch around https://github.com/rails/rails/issues/22584
+module RailsPatch22584
+  def insert_fixture(fixture, table_name)
+    # A regression with rails 4.2.5 breaks fixtures with symbol keys, specifically this one method.
+    # So, stringify keys in the fixture hash being sent to this method.
+    super(fixture.stringify_keys, table_name)
+  end
+end
+
+Rails.application.config.after_initialize do
+  ActiveRecord::Base.connection.class.include(RailsPatch22584)
+end
