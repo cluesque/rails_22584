26eaa58fa5a3ec05bef744830f4938168f0bde0c
Bill Kirtley <bill@spiffy.com>
2016-01-06 16:13:27 -0500

    subtly better test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,7 @@ class PostTest < ActiveSupport::TestCase
   test "fixtures make it to the database" do
     assert Post.count == 2
   end
+  test 'title by symbol is ok' do
+    assert Post.find_by_title!('Symbol Title')
+  end
 end
